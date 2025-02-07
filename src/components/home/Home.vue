656160828d290bf168b7fb26ae78eb4d30050808
Reptilelizard <3526380185@qq.com>
2025-02-07 18:01:52 +0800

    feat(Home): 添加服务器主页

---
+++ src/components/home/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="Home">
+    <section class="plate-1">
+      <div class="info-box">
+        <div class="title">
+          对没错这就是主页(跑
+          <p>主页设计完全没思路啊啊啊啊啊啊啊啊啊啊!</p>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+#Home {
+  width: 100%;
+}
+
+.plate-1 {
+  padding: 1px;
+  width: 100vw;
+  min-height: 91vh;
+  background: #595959;
+}
+
+.info-box {
+  width: 500px;
+  height: 200px;
+  margin: 20% auto;
+  display: flex;
+  
+}
+</style>
+
+<script setup>
+</script>
